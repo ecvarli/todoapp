343486399a467195d243965e96c4611c9f4eca83
fatiherkamdinler <fatihdinler458@gmail.com>
2024-05-09 23:22:22 +0300

    [main] created the initial app.js file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors(corsOptions))
+
+const PORT = process.env.PORT || 41001
+
+app.listen(PORT, () => {
+  if (PORT === 41001) {
+    console.log(`>>> .env file is not set correctly.`)
+  }
+  console.log(`>>> todoapp is running on http://localhost:${PORT}`)
+})
