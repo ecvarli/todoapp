b590f4a61fd4595a8b7e88edd5d9d0aa065f3ec7
ecvarli <emrecvarli@gmail.com>
2024-05-19 02:03:04 +0300

    [server-1] created getTodo, deleteTodo, updateTodo controllers

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -15,9 +15,22 @@ const getTodos = async (req, res) => {
 const getTodo = async (req, res) => {
   // Get single todo
   try {
+    const { title } = req.query
 
+    if(!title) {
+      return res.status(401).send({ error: "Title boş olamaz!!!" })
+    } else {
+      const todo = await Todo.findOne({title})
+
+      if (todo) {
+        return res.status(200).send(todo)
+      } else {
+        return res.status(404).send ({ error: "Böyle bir collection yok!!!"})
+      }
+    }
   } catch (error) {
-
+    console.log(">>> error in fn:getTodo: ", error)
+    throw new Error(error)
   }
 }
 
@@ -47,12 +60,47 @@ const createTodo = async (req, res) => {
 
 const deleteTodo = async (req, res) => {
   // Delete a todo
+  try {
+    const { title } = req.query
+
+    if(!title) {
+      return res.status(401).send({ error: "Title kısmı boş olamaz!!!" })
+    }else {
+      const result = await Todo.findOneAndDelete({title})
+
+      if(result) {
+        return res.status(200).send({ message: "Collection başarıyla silindi", result })
+      } else {
+        return res.status(500).send({error: "Collection bulunamadı!!!"})
+      }
+    }
+  } catch (error) {
+    console.log(">>> error in fn:deleteTodo ", error)
+    throw new Error(error)
+  }
 
 }
 
 const updateTodo = async (req, res) => {
   // Update a todo
+  try {
+    const {id , ...updatedTodo } = req.query
+    if (!id) {
+      return res.status(401).send ({ error: "Geçerli bir id girin!!! "})
+    } if(!updatedTodo || Object.keys(updatedTodo) === 0) {
+      return res.status(401).send ({ error: "Güncellenecek verilerini girin!!! "})
+    }
+    const update = await Todo.findByIdAndUpdate(id, updatedTodo, {new: true})
 
+    if (update) {
+      return res.status(200).send({ message: "Collection başarıyla güncellendi", update})
+    } else {
+      return res.status(404).send({ error: "Collection bulunamadı"})
+    }
+  } catch (error) {
+    console.losg(">>> error in fn:updateTodo ", error)
+    throw new Error(error)
+  }
 }
 
 module.exports = {
